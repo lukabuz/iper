929ae7c98eb38912051f24be7a51e87a11cc7503
Luka Buzaladze <lbuzaladze99@gmail.com>
2019-09-08 15:43:11 -0400

    changed location handling

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -21,15 +21,13 @@ class VisitController extends Controller
         $location = $this->get_geolocation(env('GEO_LOCATOR_API'), $ip);
         $decodedLocation = json_decode($location, true);
 
-        die($decodedLocation);
-
         $visit = new Visit;
         $visit['ip'] = $ip;
         $visit['user-agent'] = $userAgent;
         $visit['device'] = $agent->device();
         $visit['os'] = $agent->platform();
         $visit['browser'] = $agent->browser();
-        $visit['location'] = $decodedLocation->city . ', ' . $decodedLocation->country_name . ', ' . $decodedLocation->zipcode;
+        $visit['location'] = $decodedLocation['city'] . ', ' . $decodedLocation['country_name'] . ', ' . $decodedLocation['zipcode'];
         $visit->save();
 
         abort(500);
